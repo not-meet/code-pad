1c0159ce1f6cbfaad35e57656b4009b5c4698ee7
not-meet <mjmeet.279@gmail.com>
2025-01-07 05:02:25 +0530

    added the convex setup and clerk auth

---
+++ main/convex/auth.config.ts
@@ -0,0 +1,9 @@
+
+export default {
+  providers: [
+    {
+      domain: "https://tolerant-moccasin-31.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
--- main/src/app/page.tsx
+++ main/src/app/page.tsx
@@ -1,5 +1,9 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import React from "react";
+
 export default function Home() {
   return (
-    <div>home Page</div>
+    <>
+    </>
   );
 }
