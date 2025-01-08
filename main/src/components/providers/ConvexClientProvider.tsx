153e3a3b504efb80c40f8742efc80bf2945470bb
not-meet <mjmeet.279@gmail.com>
2025-01-09 01:18:01 +0530

    added auth and db setup!

---
+++ main/src/components/providers/ConvexClientProvider.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { ConvexReactClient } from 'convex/react'
+import React from 'react'
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+const ConvexClientProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CONVEX_URL!}>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+
+    </ClerkProvider>)
+}
+
+export default ConvexClientProvider
